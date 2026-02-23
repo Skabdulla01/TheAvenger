b320d00faf46be920b489f6373b03ce8495022de
Skabdulla01 <skabdullask80@gmail.com>
2026-02-24 04:38:10 +0530

    adding responsive

--- TheAvenger/src/avengerprofile.jsx
+++ TheAvenger/src/avengerprofile.jsx
@@ -21,8 +21,8 @@ function avengerprofile(props) {
       duration: 1,
       scrollTrigger:{
         trigger: txtanimation.current,
-        start: "top 60%",
-        end: "top 30%",
+        start: "top 80%",
+        end: "top 100%",
         toggleActions: "play none none reverse",
         stagger: 0.3,
         scrub: 3,
@@ -78,22 +78,22 @@ function avengerprofile(props) {
   return (
     <>
     <main className='h-screen overflow-hidden w-full pt-4 flex flex-col items-center bg-[linear-gradient(135deg,#0F172A,#1E1B4B)]'>
-      <div ref={topanimation} className="text-[4vw] leading-none font-bold">
+      <div ref={topanimation} className="text-[4vw] max-[430px]:text-[10vw] leading-none font-bold">
         {/* <h1 className='bg-linear-to-r from-[#0A2A66] via-[#0F3F99] to-[#1454CC] bg-clip-text text-transparent'>{props.name}</h1> */}
         <h1 style={{background: `linear-gradient(to right, ${props.TextStroke}, ${props.colour1}, ${props.colour2})`,WebkitBackgroundClip: "text", WebkitTextFillColor: "transparent" }}>{props.name}</h1>
       </div>
-      <div className="flex h-screen w-[90%] justify-between max-[430px]:justify-start max-[430px]:items-center" style={{ flexDirection: `${props.flexdirection}` }}>
-        <div className="relative flex justify-center items-center w-[40%]"> 
-          <div className="h-[45vw] w-full flex justify-center items-start">
-            <div ref={zoomcircle} className="w-[27vw] h-[27vw] rounded-full opacity-80" style={{background: `radial-gradient(circle, ${props.TextStroke} 0%, ${props.rgb} 100%)`}}></div>
+      <div className="flex h-screen w-[90%] justify-between max-[430px]:justify-center max-[430px]:!flex-col max-[430px]:items-center" style={{ flexDirection: `${props.flexdirection}` }}>
+        <div className="relative flex justify-center items-center max-[430px]:w-[90%] w-[40%]"> 
+          <div className="h-[45vw] max-[430px]:h-[40vh] w-full flex justify-center items-start">
+            <div ref={zoomcircle} className="w-[27vw] h-[27vw] max-[430px]:w-[60vw] max-[430px]:h-[60vw] rounded-full opacity-80" style={{background: `radial-gradient(circle, ${props.TextStroke} 0%, ${props.rgb} 100%)`}}></div>
           </div>
             <div ref={zoomanimation} className="absolute" style={{transform: props.scale}}>
               <img className='object-cover' src={props.png} alt="Captainamarica" />
             </div>
         </div>
-        <div ref={txtanimation} className="h-[80vh] max-[430px]:h-[30vh] max-[430px]:bg-green-500 w-[55%] flex flex-col justify-center">
-          <h3 className='text-[#ffffff] text-bold text-[3.5vw]' style={{ WebkitTextStroke: `2px ${props.TextStroke}` }}>{props.realname}</h3>
-          <p className='text-[#ffffff] text-[1.5vw] drop-shadow-xl'>{props.story}</p>
+        <div ref={txtanimation} className="h-[80vh] max-[430px]:h-[45vh] max-[430px]:w-[95%] w-[55%] flex flex-col justify-center">
+          <h3 className='text-[#ffffff] text-bold text-[3.5vw] max-[430px]:text-[7vw]' style={{ WebkitTextStroke: `2px ${props.TextStroke}` }}>{props.realname}</h3>
+          <p className='text-[#ffffff] text-[1.5vw] max-[430px]:text-[4vw] drop-shadow-xl'>{props.story}</p>
         </div>
       </div>
     </main>
--- TheAvenger/src/dashboard.jsx
+++ TheAvenger/src/dashboard.jsx
@@ -131,7 +131,7 @@ function dashboard() {
   })
   return (
     <>
-      {/* <Avengerprofile flexdirection="column" name={captainAmarica.hero} story={captainAmarica.story} realname={captainAmarica.realname} png={captainamaricapng} TextStroke={captainAmarica.TextStroke} colour1={captainAmarica.colour1} colour2={captainAmarica.colour2} rgb={captainAmarica.rgb} scale="scale(0.6)"/> */}
+      {/* <Avengerprofile flexdirection="row" name={captainAmarica.hero} story={captainAmarica.story} realname={captainAmarica.realname} png={captainamaricapng} TextStroke={captainAmarica.TextStroke} colour1={captainAmarica.colour1} colour2={captainAmarica.colour2} rgb={captainAmarica.rgb} scale="scale(0.6)"/> */}
 
     <button onClick={scrolltohome} ref={homebtn} style={{display: `${btn}`}} className='fixed z-200 rounded-2xl h-[4vw] w-[4vw] flex justify-center cursor-pointer items-center bottom-5 right-5 hover:bg-[linear-gradient(135deg,#0F172A,#1E1B4B)]'><svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px" fill="#FFFFFF"><path d="M452-244v-400L282-477l-42-43 241-241 241 241-42 42-168-168v402h-60Z"/></svg></button>
       <Poster poster={posters}/>
